5e726b6136740de77710b737f8c0dd266cf4c417
ARETEE <tajale01@gmail.com>
2022-09-28 10:09:17 +0545

    added new task

--- lib/components/todoTile.dart
+++ lib/components/todoTile.dart
@@ -3,36 +3,52 @@ import 'package:flutter/material.dart';
 class ToDoTile extends StatelessWidget {
   final bool checkBoxStatus;
   Function(bool?)? onChanged;
+  final String taskTitle;
 
-  ToDoTile({required this.checkBoxStatus, required this.onChanged});
+  ToDoTile({
+    required this.checkBoxStatus,
+    required this.onChanged,
+    required this.taskTitle,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(12),
-      width: double.infinity,
-      decoration: BoxDecoration(
-        color: Colors.deepPurple.shade900,
-        borderRadius: BorderRadius.circular(12),
-      ),
-      child: Row(
-        children: [
-          Checkbox(
-            value: checkBoxStatus,
-            onChanged: onChanged,
-          ),
-          Text(
-            'Do exercise',
-            style: TextStyle(
-              color: Colors.white,
-              fontFamily: 'poppins',
-              fontSize: 18,
-              decoration: checkBoxStatus
-                  ? TextDecoration.lineThrough
-                  : TextDecoration.none,
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 17),
+      child: Container(
+        padding: EdgeInsets.all(12),
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Colors.deepPurple.shade900,
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Row(
+          children: [
+            Checkbox(
+              value: checkBoxStatus,
+              onChanged: onChanged,
+              fillColor: MaterialStateProperty.resolveWith<Color>(
+                  (Set<MaterialState> states) {
+                if (states.contains(MaterialState.disabled)) {
+                  return Colors.green.withOpacity(.32);
+                }
+                return Colors.green;
+              }),
             ),
-          )
-        ],
+            Text(
+              taskTitle,
+              style: TextStyle(
+                color: Colors.white,
+                fontFamily: 'poppins',
+                fontSize: 15,
+                decoration: checkBoxStatus
+                    ? TextDecoration.lineThrough
+                    : TextDecoration.none,
+                decorationThickness: 2,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -1,17 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:todo_hive/components/todoTile.dart';
 
-class HomePage extends StatelessWidget {
-  // List of todolist;
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
 
+class _HomePageState extends State<HomePage> {
+  // List of todolist;
   List ToDoList = [
     ['Make an app', false],
     ['Complete app', false],
   ];
 
   //Method for checkbox status changed
+  void checkBoxChanged(bool? value, index) {
+    setState(() {
+      ToDoList[index][1] = !ToDoList[index][1];
+    });
+  }
 
-  void checkBoxChanged(bool? value, index) {}
+  //Method to add task
+  void addTask() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return Container();
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,10 +51,17 @@ class HomePage extends StatelessWidget {
           itemCount: ToDoList.length,
           itemBuilder: (BuildContext context, int index) {
             return ToDoTile(
-                checkBoxStatus: ToDoList[index][1],
-                onChanged: (value) => checkBoxChanged(value, index));
+              checkBoxStatus: ToDoList[index][1],
+              onChanged: (value) => checkBoxChanged(value, index),
+              taskTitle: ToDoList[index][0],
+            );
           },
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.deepPurple.shade900,
+        child: Icon(Icons.add),
       ),
     );
   }
